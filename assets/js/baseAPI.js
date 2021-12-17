8526e084858dc303f580b3c54de7d4287c7bf103
wilson <839081249@qq.com>
2021-12-17 17:25:13 +0800

    “修改了api接口”

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,7 @@
 $.ajaxPrefilter(function(options){
 
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    /* options.url = 'http://api-breakingnews-web.itheima.net' + options.url */
+    options.url = 'http://127.0.0.1:3007' + options.url
     
 
     //统一为有权限的接口，设置headers请求头
